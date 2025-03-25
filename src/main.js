36e3af15daaa92d05bbc4a2d313dc015caed7927
douuniiiia <douniabahzad@gmail.com>
2025-03-25 17:24:14 +0100

    25/03

--- src/main.js
+++ src/main.js
@@ -1,66 +1,73 @@
+console.log('test')
+
+
+
 class MusicPlayer {
   // Explication : Le constructeur est la première fonction lancée quand la Classe est instanciée. On y initialise les propriété, et appelle des fonctions.
+
+  
   constructor() {
-    // BUG : tracks est un tableau d'objets. Chaque objet représente une musique et ses proprités. Un des items du tableau n'est pas un objet.
+    console.log('constructor')
+
     // TODO DRAGGABLE : On va vouloir ajouter une propriété "img" à chaque objet, et y inscrire le lien de l'image que l'on veut charger. 
     // Pense bien à mettre tes images dans le dossier "public"
     this.tracks = [
-      { id: 1, title: "Chill Vibes", url: "track.mp4" },
-      { id: 2, title: "Summer Beats", url: "track2.mp3" },
-      id: 3, title: "Lo-Fi Relax", url: "track3.mp3"
+      { id: 1, title: "Candy", url: "/Candy Official Audio.mp3" },
+      { id: 2, title: "Distance", url: "/Yebba Distance Lyric Video.mp3" },
+      { id: 3, title: "Lo-Fi Relax", url: "track3.mp3" }
     ];
-    this.currentTrackIndex = -1; // Bug: En général, les tableaux commencent à 0
+    this.currentTrackIndex = 0; 
     this.audio = new Audio();
     this.isPlaying = false;
-    this.volume = 1.2; // BUG: C'est trop fort 
-  }
-    // BUG : Cette fonction n'est pas appelé dans le constructeur
+    this.volume = 0.5; 
     this.init();
-
+    
+  }
+    
 
 // Explication : Ici, on est en dehors du constructor, on y défini toutes les fonctions que la classe possède.
+
 
 init() {
   this.cacheDOM();
   this.bindEvents();
-  this.setupDraggable();
+  // this.setupDraggable();
   this.loadTrack();
 }
 
-// Bug: Il y a des soucis dans cette fonction : regarde bien le nom des selecteurs.
-// Bug: Regarde aussi la façon dont on déclare les variables/membres de classe. Rappelle toi que les "const" sont limité à leur portée de bloc (donc ici, àla fonction).
+
+// Bug: Regarde aussi la façon dont on déclare les variables/membres de classe. Rappelle toi que les "const" sont limité à leur portée de bloc (donc ici, à la fonction).@
 // Alors que les membres de classes (this.truc) sont appelable n'importe ou dans la classe.
 cacheDOM() {
   const playlist = document.querySelector("#playlist");
-  const playButton = document.querySelector("#play");
-  this.nextButton = document.querySelector("#nex");
+  this.playButton = document.querySelector("#play");
+  this.nextButton = document.querySelector("#next");
   this.prevButton = document.querySelector("#prev");
   this.trackTitle = document.querySelector("#track-title");
 }
 
-// Bug: Il semble que dans cette définition de fonction, on attende un paramètre, pourtant on ne l'utilise nul part. Est il vraiment utile ?
-// Bug : Il semble que des events listeners soient mal appelés. nextButton par exemple, est un élement HTML déjà défini.
-// Bug : Quel est l'évènement que l'on veut utiliser sur prevButton ? wheel ? vraiment ?
-// Bug : Son callback est également mal écrit. Regarde au dessus et en dessous comment on déclenche les fonctions de Callback
-bindEvents(item) {
+
+
+bindEvents() {
   this.playButton.addEventListener("click", () => this.togglePlay());
-  const nextButton.addEventListener("click", () => this.nextTrack()); // Bug: nextButton est undefined
-  this.prevButton.addEventListener("wheel", function () => this.prevTrack());
+  this.nextButton.addEventListener("click", () => this.nextTrack()); 
+  this.prevButton.addEventListener("click", () => this.prevTrack());
   this.audio.addEventListener("ended", () => this.nextTrack());
 }
 
-// Bug : Il manque des accolades pour décrire le corps de la fonction
-loadTrack()
+
+loadTrack(){
 if (this.currentTrackIndex < 0 || this.currentTrackIndex >= this.tracks.length) {
   console.error("Index de piste invalide");
   return;
 }
-this.audio.src = this.tracks[this.currentTrackIndex].wrongKey; // Bug: mauvais attribut
+this.audio.src = this.tracks[this.currentTrackIndex].url; 
 this.trackTitle.textContent = this.tracks[this.currentTrackIndex].title;
 // this.animateTitle();
+}
 
 togglePlay() {
-  if (isPlaying) { // BUG : La référence de isPlaying semble ne pas fonctionner, c'est un membre de classe, il faut un mot clef pour pointer dessus.
+  if (this.isPlaying) { 
     this.audio.pause();
   } else {
     this.audio.play().catch(err => console.error("Erreur de lecture :", err));
@@ -74,7 +81,7 @@ nextTrack() {
   this.currentTrackIndex = (this.currentTrackIndex + 1) % this.tracks.length;
   this.loadTrack();
   this.audio.play(); // Bug: joue même si l'audio n'est pas chargé correctement
-  this.isPlaying = 'true'; // Bug : Ici, on veut passer isPlaying a true, mais on est en train de lui passer une chaine de caractère, et pas un boolean. Donc ça ne marche pas
+  this.isPlaying = true 
 }
 
 prevTrack() {
@@ -111,6 +118,8 @@ prevTrack() {
   //     }
   // }
 }
+
+new MusicPlayer()
 
 
 // BUG : Ici, on est en dehors de la classe Music Player. 
